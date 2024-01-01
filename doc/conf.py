e36b032d0453ac6f20b119e387723b57dbf07a5a
ch-m-mueller <131595627+ch-m-mueller@users.noreply.github.com>
2024-01-01 20:16:24 +0100

    Add extensions and html_theme_options

--- doc/conf.py
+++ doc/conf.py
@@ -6,17 +6,21 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
+project = 'Power BI Example with surf spots in Portugal'
+copyright = '2023, T4D GmbH'
+author = 'Christian M. Müller'
 release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+extensions = [
+    "myst_parser",
+    "sphinx_favicon",
+    "sphinx_design"
+]
 
-extensions = ['myst_parser']
-
+    
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
@@ -24,5 +28,17 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = 'sphinx_book_theme'
+# html_static_path = ['_static']
+
+
+html_theme_options = {
+    "repository_url": "https://github.com/t4d-gmbh/powerbi-example-surfspots",
+    "repository_branch": "main",
+    "path_to_docs": "doc/",
+    "use_edit_page_button": True,
+    "use_repository_button": True,
+    "toc_title": "Content",
+    "use_sidenotes": True
+}
+
